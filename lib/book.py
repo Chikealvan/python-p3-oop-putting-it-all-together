6280a75f42071638767d4c90e70c5beb850eac94
chikealvan <nwadoziechikealvan@gmail.com>
2023-10-05 12:10:14 +0100

    first commit

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
+    def __init__(self, title, author, publication_year):
+        self.title = title
+        self.author = author
+        self.publication_year = publication_year
+        self.is_checked_out = False
+
+    def check_out(self):
+        if not self.is_checked_out:
+            self.is_checked_out = True
+            return "Book checked out successfully"
+        else:
+            return "Book is already checked out"
+
+    def check_in(self):
+        if self.is_checked_out:
+            self.is_checked_out = False
+            return "Book checked in successfully"
+        else:
+            return "Book is not checked out"
+
+    def is_available(self):
+        return not self.is_checked_out
+
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size, color):
+        self.brand = brand
+        self.size = size
+        self.color = color
+        self.is_worn = False
+
+    def wear(self):
+        if not self.is_worn:
+            self.is_worn = True
+            return "Shoe is now worn"
+        else:
+            return "Shoe is already worn"
+
+    def take_off(self):
+        if self.is_worn:
+            self.is_worn = False
+            return "Shoe is taken off"
+        else:
+            return "Shoe is not currently worn"
+
+    def is_clean(self):
+        return not self.is_worn
